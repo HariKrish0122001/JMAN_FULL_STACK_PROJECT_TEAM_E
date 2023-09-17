37c22f0379954d89ae9cc7399a9d64ba3b6aa86c
kumarcharan205 <122101017+kumarcharan205@users.noreply.github.com>
2023-09-17 18:45:37 +0530

    charan

--- frontendd/src/admin_component/navigationBar/adminnavbar.js
+++ frontendd/src/admin_component/navigationBar/adminnavbar.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import Complaint from '../../user_component/complaint'
 import { useEffect } from "react";
+import Viewrequests from "./viewrequests";
 
 function Adminnavbar() {
     const navigate= useNavigate();
@@ -16,7 +17,7 @@ function Adminnavbar() {
             <div className="sidebar">
                 <p className='welcomeAdmin'> Welcome <br/><strong>Admin</strong></p>
             
-                {/* <Complaint /> */}
+                <Viewrequests />
                 <div className="logout-btn-div">
                     <button className='logOut' onClick={handleLogout}>Logout <i class="logout fa-solid fa-arrow-right-from-bracket"></i></button>
                 </div>
---
+++ frontendd/src/admin_component/navigationBar/viewrequests.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import './requests.scss'
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+
+
+
+export default function Viewrequests({user}) {
+
+    const form = useRef();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClose = () => {
+        setShowModal(false);
+    }
+
+    const handleShow = () => {
+        setShowModal(true);
+    }
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_203o8iw', 'template_o91f0iq', form.current, '9rndwk3q5_ec5AzuH').then(
+            (result) => {
+                console.log(result.text);
+            },
+            (error) => {
+                console.log(error.text);
+            }
+        );
+    }
+
+    return (
+        <>
+            <button className="complaint" onClick={handleShow}>
+                View Requests 
+            </button>
+            <Modal show={showModal} onHide={handleClose} className="complaint">
+                
+            <table className="table table-hover table-bordered results" id="allTrainings">
+                                                <thead>
+                                                    <tr>
+                                                        
+                                                        <th >Request Id</th>
+                                                        <th >User Name</th>
+                                                        <th>User Email </th>
+                                                        <th >Message </th>
+                                   
+                                                    </tr>
+                                                    <tr className="warning no-result">
+                                                        <td colspan="4"><i className="fa fa-warning"></i> No result</td>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {/* <Users usersData={userdata} searchQuery={searchQuery} handleRegister={handleRegister} /> */}
+                                                </tbody>
+                                            </table>
+
+            </Modal>
+        </>
+    );
+}
+
+
+
+
